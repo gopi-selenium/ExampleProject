cdd5ae9e60d8cf5c51c33b90dc5ffaab31e12378
gopi-selenium <gopi.selenium@gmail.com>
2014-04-10 13:03:49 +0530

    commit for modifications in Follow.java

--- src/test/java/package1/Follow.java
+++ src/test/java/package1/Follow.java
@@ -4,12 +4,13 @@ import org.openqa.selenium.By;
 
 import org.testng.annotations.Test;
 
+
 public class Follow extends BrowserHelper {
-  
-		
-  @Test
+	
+	@Test	
   public void twitter_following() throws InterruptedException {
 	  driver.findElement(By.xpath("//table[@summary='Profile stats']/thead/tr/th[2]")).click();
 	  Thread.sleep(5000);
+	  driver.findElement(By.xpath("//input[@id='signin-email']")).isDisplayed();
   }
 }
